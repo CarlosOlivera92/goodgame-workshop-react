d324b8d3289812b49440b1bddf5cca1e582ddbea
CharlesXvr <thestuntman92@gmail.com>
2023-07-11 16:26:41 -0300

    error in home with itemListContainer fixed

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,15 +4,18 @@ import codbo from '../../assets/img/codbo.jpg';
 import rdr2 from "../../assets/img/rdr2.webp";
 import wrc from "../../assets/img/wrc.jpg";
 import './style.css';
+import CategoryContext from '../../utils/context/filter-context';
 
 const Home = () => {
   const carouselImages = [codbo, rdr2, wrc];
 
   return (
-    <div className="container-fluid m-0 p-0">
-      <Carousel images={carouselImages} />
-      <ItemListContainer/>
-    </div>
+    <CategoryContext.Provider value={{ selectedCategories: [], setSelectedCategories: () => {} }}>
+      <div className="container-fluid m-0 p-0">
+        <Carousel images={carouselImages} />
+        <ItemListContainer/>
+      </div>
+    </CategoryContext.Provider>
   );
 };
 
